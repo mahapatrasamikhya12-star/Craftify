f8cbfecb0be53135df3d4b15adfb1ba091eab8df
mahapatrasamikhya12-star <mahapatrasamikhya12@gmail.com>
2026-05-09 21:38:58 +0530

    Update urls.py

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -3,5 +3,8 @@ from rest_framework_simplejwt.views import TokenRefreshView
 from.views import LoginView
 
 urlpatterns = [
-        path('login/', LoginView.as_view(), name='login'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('profile/', ProfileView.as_view(), name='profile'),
 ]
