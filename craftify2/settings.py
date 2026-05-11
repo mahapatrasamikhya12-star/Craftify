7ac3004a666cb7a9098cc6d7f823214d5a671ddd
mahapatrasamikhya12-star <mahapatrasamikhya12@gmail.com>
2026-05-11 10:29:03 +0530

    Update settings.py

--- craftify2/settings.py
+++ craftify2/settings.py
@@ -1,25 +1,38 @@
 from pathlib import Path
 from datetime import timedelta
-from decouple import config 
+from decouple import config
+
+# Base directory
 BASE_DIR = Path(__file__).resolve().parent.parent
-SECRET_KEY = config('SECRET_KEY',default='django-insecure-craftify2-secret-key-change-in-production')
-DEBUG = config('DEBUG',default=True, cast=bool)
-ALLOWED_HOSTS =['*']
-INSTALLED_APPS =[
+
+# Security
+SECRET_KEY = config(
+    'SECRET_KEY',
+    default='django-insecure-craftify2-secret-key-change-in-production'
+)
+
+DEBUG = config('DEBUG', default=True, cast=bool)
+
+ALLOWED_HOSTS = ['*']
+
+# Installed apps
+INSTALLED_APPS = [
+    # Django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-     
-     #third party
+
+    # Third-party apps
     'rest_framework',
     'rest_framework_simplejwt',
+    'rest_framework_simplejwt.token_blacklist',
     'corsheaders',
     'django_filters',
-    
-    #Local apps
+
+    # Local apps
     'apps.users',
     'apps.products',
     'apps.orders',
@@ -27,68 +40,114 @@ INSTALLED_APPS =[
     'apps.reviews',
     'apps.payments',
 ]
-MIDDLEWARE =[
+
+# Middleware
+MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+
     'corsheaders.middleware.CorsMiddleware',
+
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
 ROOT_URLCONF = 'craftify2.urls'
+
+# Templates
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS':[],
+        'DIRS': [],
         'APP_DIRS': True,
-        'OPTIONS':{
+        'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
+
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-           ],
+            ],
         },
     },
 ]
+
 WSGI_APPLICATION = 'craftify2.wsgi.application'
+
+# Database
 DATABASES = {
-    'default':{
+    'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME':BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-AUTH_PASSWORD_VALIDATORS =[
-    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
+
+# Password validation
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME':
+        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME':
+        'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME':
+        'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME':
+        'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
 ]
-LANGUAGE_CODE ='en-us'
-TIME_ZONE ='UTC'
-USE_I18N =True
+
+# Language
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
 USE_TZ = True
-STATIC_URL ='/static/'
+
+# Static files
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-MEDIA_URL ='/media/'
-MEDIA_ROOT =BASE_DIR /'media'
+
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Custom user model
 AUTH_USER_MODEL = 'users.CustomUser'
-REST_FRAMEWORK ={
-    'DEFAULT_AUTHENTICATION_CLASSES':(
-     'rest_framework_simplejwt.authentication.JWTAuthentication',
+
+# REST Framework
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
-'DEFAULT_FILTER_BACKENDS':(
-    'django_filters.rest_framework.DjangoFilterBackend',
-),
-'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-'PAGE_SIZE':12,
+
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ),
+
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.PageNumberPagination',
+
+    'PAGE_SIZE': 12,
 }
+
+# JWT Settings
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': True,
-    }
-CORS_ALLOW_ALL_ORIGINS = True    craftify/setting.py
+}
+
+# CORS
+CORS_ALLOW_ALL_ORIGINS = True
