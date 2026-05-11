bcdfae7e9e122d6fc07704733190d78e9e79f23a
mahapatrasamikhya12-star <mahapatrasamikhya12@gmail.com>
2026-05-11 10:25:09 +0530

    Update settings.py

--- craftify2/settings.py
+++ craftify2/settings.py
@@ -1,30 +1,32 @@
 from pathlib import Path
 from datetime import timedelta
-from decouple import config 
-
+from decouple import config
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECRET_KEY = config('SECRET_KEY',
+    default='django-insecure-craftify2-secret-key-change-in-production')
 
-SECRET_KEY = config('SECRET_KEY',default='django-insecure-craftify2-secret-key-change-in-production')
-DEBUG = config('DEBUG',default=True, cast=bool)
-ALLOWED_HOSTS =['*']
+DEBUG = config('DEBUG', default=True, cast=bool)
 
-INSTALLED_APPS =[
+ALLOWED_HOSTS = ['*']
+
+INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-     
-     #third party
+
+    # third party
     'rest_framework',
     'rest_framework_simplejwt',
+    'rest_framework_simplejwt.token_blacklist',  # ← ADD for logout blacklist
     'corsheaders',
     'django_filters',
-    
-    #Local apps
+
+    # local apps
     'apps.users',
     'apps.products',
     'apps.orders',
@@ -33,10 +35,9 @@ INSTALLED_APPS =[
     'apps.payments',
 ]
 
-
-
-MIDDLEWARE =[
+MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',   # ← ADD for Render static files
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -46,21 +47,20 @@ MIDDLEWARE =[
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-
 ROOT_URLCONF = 'craftify2.urls'
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS':[],
+        'DIRS': [],
         'APP_DIRS': True,
-        'OPTIONS':{
+        'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-           ],
+            ],
         },
     },
 ]
@@ -68,51 +68,54 @@ TEMPLATES = [
 WSGI_APPLICATION = 'craftify2.wsgi.application'
 
 DATABASES = {
-    'default':{
+    'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME':BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
-AUTH_PASSWORD_VALIDATORS =[
+AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
     {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
     {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE     = 'UTC'
+USE_I18N      = True
+USE_TZ        = True
 
-LANGUAGE_CODE ='en-us'
-TIME_ZONE ='UTC'
-USE_I18N =True
-USE_TZ = True
-
-
-STATIC_URL ='/static/'
+STATIC_URL  = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-MEDIA_URL ='/media/'
-MEDIA_ROOT =BASE_DIR /'media'
+MEDIA_URL   = '/media/'
+MEDIA_ROOT  = BASE_DIR / 'media'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-AUTH_USER_MODEL = 'users.CustomUser'
+AUTH_USER_MODEL    = 'users.CustomUser'
 
-
-REST_FRAMEWORK ={
-    'DEFAULT_AUTHENTICATION_CLASSES':(
-     'rest_framework_simplejwt.authentication.JWTAuthentication',
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
-'DEFAULT_FILTER_BACKENDS':(
-    'django_filters.rest_framework.DjangoFilterBackend',
-),
-'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-'PAGE_SIZE':12,
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 12,
+    'DEFAULT_PARSER_CLASSES': [       # ← fixes DRF browser JSON issue
+        'rest_framework.parsers.JSONParser',
+        'rest_framework.parsers.FormParser',
+        'rest_framework.parsers.MultiPartParser',
+    ],
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
-    'ROTATE_REFRESH_TOKENS': True,
-    }
+    'ACCESS_TOKEN_LIFETIME':  timedelta(days=7),   # ← 7 days
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=30),  # ← 30 days
+    'ROTATE_REFRESH_TOKENS':  True,
+}
 
 CORS_ALLOW_ALL_ORIGINS = True
